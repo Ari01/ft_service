6d737300d198a37b2cdec55b9a5493d85d59865f
dndn <denis.c1@live.fr>
2021-04-24 16:14:27 +0200

    mysql runs but for now wp cant connect to it

--- setup.sh
+++ setup.sh
@@ -14,13 +14,12 @@
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-	kubectl get pod -n metallb-system -o wide
 	kubectl get configmap kube-proxy -n kube-system -o yaml | \
 	sed -e "s/strictARP: false/strictARP: true/"
+	kubectl get pod -n metallb-system -o wide
 	#kubectl get configmap kube-proxy -n kube-system -o yaml | \
 	#kubectl apply -f - -n kube-system
 
 # KUBERNETES CONF
-	eval $(minikube docker-env)
 	sh build.sh
 	kubectl apply -k srcs/config/. 
--- srcs/mysql/mysql.sh
+++ srcs/mysql/mysql.sh
@@ -1,9 +1,14 @@
 mkdir /run/openrc
 touch /run/openrc/softlevel
+chown -R mysql /var/lib/mysql
+chmod -R 777 /var/lib/mysql
 
 /etc/init.d/mariadb setup
 rc-status
 /etc/init.d/mariadb start
 
 mysql -u root -puser42 < /home/docker/script/database.sh
-sh
+#/etc/init.d/mysqld stop 
+mysqladmin -u root -puser42 shutdown
+exec mysqld --user=root
+#exec /usr/bin/mysqld #--user=mysql --console
