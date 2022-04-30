9ee622e543073660da368a13cb5b296ac063cfc7
denis <denis.c1@live.fr>
2022-04-30 16:23:28 +0200

    ssh ok

--- srcs/nginx/srcs/nginx.sh
+++ srcs/nginx/srcs/nginx.sh
@@ -1,13 +1,15 @@
 # SSL
-cd /etc/nginx/certificate
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key < /home/docker/script/ssl.sh
+#cd /etc/nginx/certificate
+#openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key < /home/docker/script/ssl.sh
 
 # init
-#openrc
-#touch /run/openrc/softlevel
+openrc
+touch /run/openrc/softlevel
 
 # IP CONFIG
-IP=$(ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//')
+#IP=$(ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//')
 #sed -i -e "s/server_name _/server_name $IP/g" /etc/nginx/nginx.conf
-echo $IP
+#echo $IP
+#/etc/init.d/sshd
+service sshd start
 nginx -g "daemon off;"
