69f441354694bcf6bd61d72e09ef79cf7cf1ef4c
denden <denis.c1@live.fr>
2022-06-06 08:11:31 +0200

    grafana doesnt work

--- srcs/scripts/install_minikube.sh
+++ srcs/scripts/install_minikube.sh
@@ -1,17 +1,17 @@
 # KUBERNETES
 	#DEPENDENCIES
 	sudo apt-get update
-	sudo apt-get install -y apt-transport-https ca-certificates curl
+	#sudo apt-get install -y apt-transport-https ca-certificates curl
 
 	# GPG KEY
-	sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+	#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 	# KUBERNETES DIR
-	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 	
 	# KUBERNETES INSTALL
-	sudo apt-get update
-	sudo apt-get install -y kubectl
+	#sudo apt-get update
+	#sudo apt-get install -y kubectl
 
 	# MINIKUBE INSTALL
 	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
